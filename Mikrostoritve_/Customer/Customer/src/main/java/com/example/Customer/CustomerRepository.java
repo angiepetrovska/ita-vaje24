eab835b42aaa2b3720780b21993cc3cb34c87d5b
apetrovska <44358450+angiepetrovska@users.noreply.github.com>
2024-08-01 20:39:37 +0200

    Delete Mikrostoritve_ directory

--- Mikrostoritve_/Customer/Customer/src/main/java/com/example/Customer/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.Customer;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.List;
-
-public interface CustomerRepository extends MongoRepository<Customer, String> {
-}
